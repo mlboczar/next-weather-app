135c2f68a2d51579c7148bb938acfd76b78deb5a
mlboczar <mlboczar@gmail.com>
2023-12-05 11:40:52 -0500

    Added icon

--- src/app/components/Weather/Weather.tsx
+++ src/app/components/Weather/Weather.tsx
@@ -2,16 +2,32 @@ import { WeatherType } from "@/types/types"
 import styles from "./Weather.module.css"
 
 const Weather = ({weather}:WeatherType): JSX.Element => {
+    let imageUrl;
+    if (weather && weather.main) {
+        imageUrl = `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`
+    }
+
     return (
         <div className={styles.main}>
         {weather && weather.main ? (
             <div className={styles.container}>
-                <h2>{weather?.name}</h2>
+                <div className={styles.header}>
+                    <h2>{weather?.name}</h2>
+                    <img className={styles.image} src={imageUrl} alt="weather-icon"/>
+                </div>
         
-                <div className={styles.element}>Description: {weather?.weather[0].description}</div>
-                <div className={styles.element}>Current temp: {weather?.main.temp}°F</div>
-                <div className={styles.element}>Feels like: {weather?.main.feels_like}°F</div>
-                <div className={styles.element}>Wind speed: {weather?.wind.speed}mph</div>
+                <div className={styles.element}>
+                    Description: {weather?.weather[0].description}
+                </div>
+                <div className={styles.element}>
+                    Current temp: {weather?.main.temp}°F
+                </div>
+                <div className={styles.element}>
+                    Feels like: {weather?.main.feels_like}°F
+                </div>
+                <div className={styles.element}>
+                    Wind speed: {weather?.wind.speed}mph
+                </div>
             </div>
         ) : (
             <h3>Loading...</h3>
